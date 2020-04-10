ef2044482e0520b78932250e6cdc55e1893a7fad
Nisan Sabag <nisans@google.com>
2020-04-10 15:59:52 +0300

    adding build

---
+++ build/precache-manifest.c8e0988f933aeac97728774fb0d40719.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59266fe5f589e8b1f586a6c9f809b966",
+    "url": "/react-tv-app/index.html"
+  },
+  {
+    "revision": "9def3d643bded028dcf0",
+    "url": "/react-tv-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c6876cd5f65cac4e802f",
+    "url": "/react-tv-app/static/css/main.b23b64ed.chunk.css"
+  },
+  {
+    "revision": "9def3d643bded028dcf0",
+    "url": "/react-tv-app/static/js/2.13fe180b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tv-app/static/js/2.13fe180b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6876cd5f65cac4e802f",
+    "url": "/react-tv-app/static/js/main.993acd8c.chunk.js"
+  },
+  {
+    "revision": "b7bded017f9a650b81fb",
+    "url": "/react-tv-app/static/js/runtime-main.856450d3.js"
+  },
+  {
+    "revision": "9a8b0596eaa330bd6b4483b1b8182dc8",
+    "url": "/react-tv-app/static/media/Email.9a8b0596.png"
+  },
+  {
+    "revision": "6b09800dc6fa4b430952fcda510daea1",
+    "url": "/react-tv-app/static/media/logo.6b09800d.png"
+  }
+]);
